214f5e64dbdb61bbd895e43ecb5742900954816e
gminteer <gminteer@gmail.com>
2020-10-18 19:09:26 -0400

    Revert "Revert "Revert "tweaked header/footer, styled contact, started to style gallery & project"""
    This reverts commit 4bf422252ced7c0a6c01e2868a70cda7cbc3fe5f.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 import Project from 'components/Project';
 import projects from 'assets/data/projects.json';
-import styles from './Gallery.module.scss';
 
 export default () => (
-  <main className={styles.Gallery}>
+  <main>
     {projects.map((project, index) => (
       <Project project={project} key={index} />
     ))}
