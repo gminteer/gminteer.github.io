b80a354b3a8a23039bbee1cb37090faa05abfaaa
gminteer <gminteer@gmail.com>
2020-10-19 03:16:32 -0400

    icons, style tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import data from 'assets/data/footer.json';
 import styles from './Footer.module.scss';
 
-export default () => (
-  <footer className={styles.Footer}>
-    <ul>
-      {data.map((item, index) => (
-        <li key={index}>
-          <a href={item.href}>{item.name}</a>
-        </li>
-      ))}
-    </ul>
-  </footer>
-);
+export default function Footer() {
+  const [icons, setIcons] = useState({});
+  useEffect(() => {
+    async function loadIcons() {
+      const data = await import('assets/data/footer.json');
+      const iconRefs = await Promise.all(
+        data.default.map((item) => import(`assets/icons/${item.icon}`))
+      );
+      const iconEntries = data.default.map((item, index) => [
+        item.name,
+        iconRefs[index].default,
+      ]);
+      setIcons(Object.fromEntries(iconEntries));
+    }
+    loadIcons();
+  }, []);
+
+  return (
+    <footer className={styles.Footer}>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            <a href={item.href}>
+              <img
+                className={
+                  item.mono ? `${styles.icon} ${styles.mono}` : styles.icon
+                }
+                src={icons[item.name]}
+                alt={item.name}
+              />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 import styles from './Navigation.module.scss';
 
-export default ({ tabs, setCurrentTab }) => (
-  <nav className={styles.Navigation}>
-    <ul>
-      {tabs.map((tab, index) => (
-        <li key={index}>
-          <h2>
-            <a
-              href={`#${tab.toLowerCase()}`}
-              onClick={() => setCurrentTab(tab)}
-            >
-              {tab}
-            </a>
-          </h2>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+export default function Navigation({ title, tabs, currentTab, setCurrentTab }) {
+  return (
+    <nav className={styles.Navigation}>
+      <ul>
+        {tabs.map((tab, index) => (
+          <li
+            key={index}
+            className={tab === currentTab ? styles.activeTab : undefined}
+          >
+            <h2>
+              <a
+                href={`#${tab.toLowerCase()}`}
+                onClick={() => setCurrentTab(tab)}
+              >
+                {tab}
+              </a>
+            </h2>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
