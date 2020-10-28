1b08e2d314c756d15d64f3e01a797e83c9944f02
gminteer <gminteer@gmail.com>
2020-10-28 15:42:06 -0400

    separated pages into their own folder

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import Background from 'components/Background';
-import Fortune from 'components/Fortune';
+import Fortune from 'pages/Fortune';
 
 const Pages = {
-  about: React.lazy(() => import('components/About')),
-  projects: React.lazy(() => import('components/Gallery')),
-  contact: React.lazy(() => import('components/Contact')),
-  resume: React.lazy(() => import('components/Resume')),
+  about: React.lazy(() => import('pages/About')),
+  projects: React.lazy(() => import('pages/Gallery')),
+  contact: React.lazy(() => import('pages/Contact')),
+  resume: React.lazy(() => import('pages/Resume')),
 };
 
 export default function App() {
