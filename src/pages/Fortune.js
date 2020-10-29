88224cbd1e131228d1310ff702fc71b20b781484
gminteer <gminteer@gmail.com>
2020-10-28 21:34:21 -0400

    got rid of pos: fixed and used flexbox for main layout like a sane person

--- src/pages/Fortune.js
+++ src/pages/Fortune.js
@@ -10,6 +10,17 @@ export default function Fortune() {
   useEffect(() => {
     async function applyFortune() {
       setMessage(await cowsayFortune());
+      // This will spit out 80 columns + 15 lines of numbers to help with tweaking formatting
+      // setMessage(
+      //   say({
+      //     text: Array(15)
+      //       .fill()
+      //       .map((i) => [...Array(10).keys()].join('').repeat(8) + '\n')
+      //       .join(''),
+      //     e: '??',
+      //     n: true,
+      //   })
+      // );
     }
     applyFortune();
     document.title = '~gminteer/';
