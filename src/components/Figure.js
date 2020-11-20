a9a2f10f91b7cf00cc001433208ca32f6ca5de57
gminteer <gminteer@gmail.com>
2020-11-19 20:05:43 -0500

    figure component change should also go here since the site breaks without it

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 import styles from './Figure.module.scss';
 import placeholder from 'assets/img/placeholder.jpg';
 
-export default function Figure({ title, body, imgCfg }) {
+export default function Figure({ style, title, body, imgCfg }) {
   const [img, setImg] = useState();
-
+  const className = style ? `${styles.Figure} ${style}` : styles.Figure;
   useEffect(() => {
     async function getImg() {
       const imgData = await import(`assets/img/${imgCfg.src}`);
@@ -15,8 +15,8 @@ export default function Figure({ title, body, imgCfg }) {
   }, [imgCfg.src, img]);
 
   return (
-    <figure className={styles.Figure}>
-      <div class={styles.imgContainer}>
+    <figure className={className}>
+      <div className={styles.imgContainer}>
         <img src={img ? img : placeholder} alt={imgCfg.alt} />
       </div>
       <figcaption>
